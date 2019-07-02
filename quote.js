e5947350f20ce63fc7f1cde3ce85dd6f15579cf3
Jay <jay@MWCSB2s-MacBook-Pro.local>
2019-07-02 11:18:23 -0700

    Adding Non-blocking promise to write file

--- quote.js
+++ quote.js
@@ -1,16 +1,31 @@
 const csv = require("fast-csv");
 const writeFile = require('write');
 
-(async() => {
+// (async() => {
+//     let count = 0;
+//     let newTxt = ''
+//     const stream = await csv.fromPath("prod_1.csv").on("data", function(data){
+//         newTxt = newTxt+ "'" + data[0].trim() + "',\n"
+//         count++;
+//     }).on("end", function(){
+//         // Remove the comma and new line character for the last one
+//         writeFile.sync('foo.txt', newTxt.slice(0, -2)); 
+//         console.log(`Task completed with ${count} records processed.` );
+//     }); 
+// })()
+
+
+// Promise, non-blocking
+(async () => {
     let count = 0;
     let newTxt = ''
-    const stream = await csv.fromPath("prod_1.csv").on("data", function(data){
-        newTxt = newTxt+ "'" + data[0].trim() + "',\n"
-        // console.log(newTxt)
+    const stream = await csv.fromPath("prod.csv").on("data", data => {
+        newTxt = newTxt + "'" + data[0].trim() + "',\n"
         count++;
-    }).on("end", function(){
-        // Remove the comma and new line character for the last one
-        writeFile.sync('foo.txt', newTxt.slice(0, -2)); 
-        console.log(`Task completed with ${count} records processed.` );
-    }); 
+    }).on("end", () => {
+        writeFile.promise('foo.txt', newTxt.slice(0, -2))
+            .then(() => {
+                console.log(`Task completed with ${count} records processed.`);
+            });
+    });
 })()
