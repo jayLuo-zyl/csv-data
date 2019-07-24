9ef14f225f491268b0b76cd46ddfe70b9809c809
Jay <jay@MWCSB2s-MacBook-Pro.local>
2019-07-24 12:06:21 -0700

    Replace the legacy csv function by csv.parseFile

--- quote.js
+++ quote.js
@@ -4,7 +4,7 @@ const writeFile = require('write');
 // (async() => {
 //     let count = 0;
 //     let newTxt = ''
-//     const stream = await csv.fromPath("prod_1.csv").on("data", function(data){
+//     const stream = await csv.fromPath("prod.csv").on("data", function(data){
 //         newTxt = newTxt+ "'" + data[0].trim() + "',\n"
 //         count++;
 //     }).on("end", function(){
@@ -12,20 +12,21 @@ const writeFile = require('write');
 //         writeFile.sync('foo.txt', newTxt.slice(0, -2)); 
 //         console.log(`Task completed with ${count} records processed.` );
 //     }); 
-// })()
+// })().catch(err => setImmediate(() => { throw err }));
 
 
 // Promise, non-blocking
 (async () => {
     let count = 0;
     let newTxt = ''
-    const stream = await csv.parseFile("prod.csv").on("data", data => {
+    await csv.parseFile("prod.csv").on("data", data => {
         newTxt = newTxt + "'" + data[0].trim() + "',\n"
         count++;
     }).on("end", () => {
         writeFile.promise('foo.txt', newTxt.slice(0, -2))
             .then(() => {
-                console.log(`Task completed with ${count} records processed.`);
-            });
-    });
-})().catch(err=> setImmediate(()=>{ throw err }));
+                console.log(`Task completed with ${count} records processed.`)
+            })
+            .catch(err => console.log(err))
+    }).on('error', error => console.error(error));
+})().catch(err => setImmediate(() => { throw err }));
